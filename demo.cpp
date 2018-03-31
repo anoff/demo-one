90ee87837e533fe82d18165443eb69fd9c80d8a7
anoff <offenhaeuser@gmail.com>
2018-03-31 17:03:49 +0200

    :scissors: refactor: fix brackets lol

--- demo.cpp
+++ demo.cpp
@@ -3,8 +3,7 @@
 // Precomputed (or otherwise) gradient vectors at each grid node
 float Gradient[YRES][XRES][2];
 
-void put_pixel32(SDL_Surface *surface, int x, int y, uint32_t pixel)
-{
+void put_pixel32(SDL_Surface *surface, int x, int y, uint32_t pixel) {
 	Uint32 *pixels = reinterpret_cast<uint32_t*>(surface->pixels);
 	pixels[(y * surface->w) + x] = pixel;
 }
@@ -12,15 +11,11 @@ void put_pixel32(SDL_Surface *surface, int x, int y, uint32_t pixel)
 // https://en.wikipedia.org/wiki/Perlin_noise#Pseudocode
 // Function to linearly interpolate between a0 and a1
 // Weight w should be in the range [0.0, 1.0]
-float lerp(float a0, float a1, float w)
-{
+float lerp(float a0, float a1, float w) {
 	return (1.0 - w)*a0 + w*a1;
 }
 // Computes the dot product of the distance and gradient vectors.
-float dotGridGradient(int ix, int iy, float x, float y)
-{
-
-
+float dotGridGradient(int ix, int iy, float x, float y) {
 		// Compute the distance vector
 		float dx = x - (float)ix;
 		float dy = y - (float)iy;
@@ -30,9 +25,7 @@ float dotGridGradient(int ix, int iy, float x, float y)
 }
 
 // Compute Perlin noise at coordinates x, y
-float perlin(float x, float y)
-{
-
+float perlin(float x, float y) {
 		// Determine grid cell coordinates
 		int x0 = floor(x);
 		int x1 = x0 + 1;
@@ -57,17 +50,14 @@ float perlin(float x, float y)
 		return value;
 }
 
-float clamp(float val, float low, float high)
-{
+float clamp(float val, float low, float high) {
 	const float t = val < low ? low : val;
 	return t > high ? high : t;
 }
-uint32_t color(const int r, const int g, const int b)
-{
+uint32_t color(const int r, const int g, const int b) {
 	return 0xff000000 | (r << 16) | (g << 8) | b;
 }
-uint32_t hot_cold(float f)
-{
+uint32_t hot_cold(float f) {
 	clamp(f, 0.f, 1.f);
 	f = (2.f * f) - 1.f;
 	float r = clamp(1.5f - abs(2.f * f - 1.f), 0.f, 1.f);
@@ -76,8 +66,7 @@ uint32_t hot_cold(float f)
 	return color(static_cast<int>(255.f * r), static_cast<int>(255.f * g), static_cast<int>(255.f * b));
 }
 
-void demo_init()
-{
+void demo_init() {
 	for (int y = 0; y < YRES; y++) {
 		for (int x = 0; x < XRES; x++) {
 			float fx = rand() % 1000 - 500;
@@ -91,11 +80,10 @@ void demo_init()
 	}
 }
 
-void demo_do(SDL_Surface *surface)
-{
+void demo_do(SDL_Surface *surface) {
 	for (int y = 0; y<surface->h; y++) {
 		for (int x = 0; x<surface->w; x++) {
-			float p = perlin(x/(float)33, y/(float)33);
+			float p = perlin(x/(float)33/ASPECT, y/(float)33);
 			put_pixel32(surface, x, y, hot_cold(p + .5));
 		}
 	}
